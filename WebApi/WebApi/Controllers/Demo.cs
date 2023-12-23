aac5285e4dab98506ad4439ca52d7c747955b81d
areeyaswnth <teen-areeya@hotmail.com>
2023-12-23 11:27:22 +0700

    Update:,Net Web API

---
+++ WebApi/WebApi/Controllers/Demo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    
+    public class DemoController:ControllerBase
+    {
+        [Route("Countries")]
+        [HttpGet]
+        public List<string> GetCourntries(string match)
+        {
+            var items = new List<string>
+            {
+                "Thailand",
+                "USA",
+                "Korea",
+                "Japan"
+            };
+            if(match == null){
+                return items;
+            }
+
+            var results = items.Where(item => item.Contains(match));
+
+            return results.ToList();
+        }
+    }
+}
